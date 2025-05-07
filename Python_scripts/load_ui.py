431fb0c965894eee38242543284a7b03a7a80d5b
Daniele Olivieri <danieledaniele1988@gmail.com>
2025-05-07 16:25:22 +0200

    Aggiunta GUI Funzionale

---
+++ Python_scripts/load_ui.py
@@ -0,0 +1,68 @@
+import sys
+import os,fnmatch
+from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt6.uic import loadUi
+from read_serial import Serial_reader
+import pyqtgraph as pg
+
+
+
+class MainUI(QMainWindow):
+    def __init__(self):
+        super(MainUI,self).__init__()
+        loadUi("GUI/SerialReaderGUI.ui", self)
+        #print(list_ACM)
+
+        self.PortSelect.addItem("Select Port")
+        self.RescanBTN.clicked.connect(self.scan_ports)
+        self.scan_ports()
+            #print("/dev/"+element)
+        #self.PortSelect.editTextChanged.connect(self.updatePort)
+        self.PortSelect.currentIndexChanged.connect(self.updatePort)
+        #More functions, clicks, connections.. here
+        self.AcquireBTN.clicked.connect(self.Acquire)
+
+        # Graph init
+        self.PlotWidget.setBackground('w')
+        self.PlotWidget.setTitle("Serial Data")
+        self.PlotWidget.setLabel('left', 'Y-Axis')
+        self.PlotWidget.setLabel('bottom', 'X-Axis')
+        self.PlotWidget.showGrid(x=True, y=True)
+
+    def scan_ports(self):
+        self.PortSelect.clear()
+        self.PortSelect.addItem("Select Port")
+        list_ACM = fnmatch.filter(os.listdir("/dev/"),"*ACM*")
+        for element in list_ACM:
+            self.PortSelect.addItem(element,"/dev/"+element)
+        self.PortSelect.setCurrentIndex(len(list_ACM))
+
+    def updatePort(self):
+        #print(self.PortSelect.currentText())
+        #print(self.PortSelect.currentData())
+        pass
+
+    def Acquire(self):
+        if ui.PortSelect.currentText() == "Select Port":
+            QMessageBox.warning(self,"Port Error","Device not selected")
+            #print("Select a port")
+        else:
+            serial = Serial_reader(ui.PortSelect.currentData(),int(self.BaudLineEdit.text()),int(self.SampleCountLine.text()))
+            #serial.read()
+            self.plot(serial.read())
+
+    def plot(self,data):
+        #self.PlotWidget = pg.PlotWidget()
+        self.PlotWidget.clear()
+        self.PlotWidget.addLegend()
+        self.PlotWidget.plot(data,pen='r',name="Test")
+        self.PlotWidget.show()
+
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+    ui = MainUI()
+    ui.show()
+    app.exec()
+    
+
--- Python_scripts/read_serial.py
+++ Python_scripts/read_serial.py
@@ -1,48 +1,50 @@
-import serial
+class Serial_reader():
+    def __init__(self,port,baud,count):
+        import serial
+        self.count = count
+        self.port = port
+        self.baud = baud
+        self.pre = []
+        self.post = []
+        self.pre_total=[]
+        self.ser = serial.Serial(port, baudrate=baud, timeout=3.0)
+        self.counter = 0
+        #while True:
 
-pre = []
-post = []
-pre_total=[]
-ser = serial.Serial("COM3", baudrate=9600, timeout=3.0)
-count = 0
-#while True:
-for line in ser.read(1000):
-    if (chr(line) == (('\r'))):
-        count = count+1
-        pass
-        #print("\n")
-    else:
-        if (count > 0):
-            print(str(count) + str(': ') + str(bin(line)) )
-            #print((line))
-            if ((count%2)!=0):#Se dispari
-                post.append(bin(line))
-                count = count+1
+    def read(self):
+        for line in self.ser.read(self.count):
+            if (chr(line) == (('\r'))):
+                self.counter = self.counter+1
+                pass
+                #print("\n")
             else:
-                pre.append(bin(line))
-            
+                if (self.counter > 0):
+                    print(str(self.counter) + str(': ') + str(bin(line)) )
+                    #print((line))
+                    if ((self.counter%2)!=0):#Se dispari
+                        self.post.append(bin(line))
+                        self.counter = self.counter+1
+                    else:
+                        self.pre.append(bin(line))
+                    
 
-if len(pre)>len(post):
-    pre.pop()
-elif len(post)>len(pre):
-    post.pop()
+        if len(self.pre)>len(self.post):
+            self.pre.pop()
+        elif len(self.post)>len(self.pre):
+            self.post.pop()
 
+        for element in self.pre:
+            self.pre_total.append(element + '00000000')
 
-#if len(pre[1])>6:
-#    a = pre
-#    pre = post
-#    post = a
+        somma = []
+        for i in range(0,len(self.post)):
+            somma.append(int(self.pre_total[i],2)+int(self.post[i],2))
 
-for element in pre:
-    pre_total.append(element + '00000000')
+        return somma
 
-#print(pre_total)
-#print(post)
-somma = []
-for i in range(0,len(post)):
-    somma.append(int(pre_total[i],2)+int(post[i],2))
-
-print(somma)
+if __name__=="__main__":
+    read = Serial_reader("COM3",9600,1000)
+    print(read)
 
 #for element in pre_total:
 #    print(int(element,2))
