b64e9b036ad6ca257557b40098f23de62a917e2e
Daniele Olivieri <danieledaniele1988@gmail.com>
2025-05-20 09:26:18 +0200

    Corretta dichiarazione implicita

--- Core/Src/timers.c
+++ Core/Src/timers.c
@@ -1,24 +1,5 @@
 #include <my_structures.h>
 
-void init_timers(){
-	RCC->TIM2EN = 1; //enable timer 6
-	RCC->TIM3EN = 1; //Enable timer 3
-	RCC->TIM4EN=1; //Enable timer 4
-
-	TIM4->PSC = 63;
-	TIM4->ARR=62500;
-	TIM4->CEN=1;
-
-	TIM3->PSC=79; // 8 MHz to 100khz
-
-	TIM2->PSC=7999; // 8 MHz to 1Khz
-	//TIM2->CEN = 1;
-	//TIM6->ARR = (unsigned int) 62500; //500ms ovvero 4000000 count
-	//TIM6->PSC = (unsigned int) 63;
-	delay_us(1);
-	delay_ms(1);
-}
-
 void delay_us(int us){
 	if (us>1){
 	TIM3->ARR = us-1;
@@ -49,6 +30,25 @@ void delay_ms(int ms){
 	TIM2->UIF=0;
 	TIM2->CEN=0;
 
+}
+
+void init_timers(){
+	RCC->TIM2EN = 1; //enable timer 6
+	RCC->TIM3EN = 1; //Enable timer 3
+	RCC->TIM4EN=1; //Enable timer 4
+
+	TIM4->PSC = 63;
+	TIM4->ARR=62500;
+	TIM4->CEN=1;
+
+	TIM3->PSC=79; // 8 MHz to 100khz
+
+	TIM2->PSC=7999; // 8 MHz to 1Khz
+	//TIM2->CEN = 1;
+	//TIM6->ARR = (unsigned int) 62500; //500ms ovvero 4000000 count
+	//TIM6->PSC = (unsigned int) 63;
+	delay_us(1);
+	delay_ms(1);
 }
 
 
